5d812ed56b2f379e2249ba209bd2f261a310d9b4
kyle-f-brooks <kylefrankbrooks1997@gmail.com>
2018-08-20 09:25:22 +0100

    Created a register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input
+      type="email"
+      name="email"
+      placeholder="email"/>
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"/>
+    <br>
+    <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
