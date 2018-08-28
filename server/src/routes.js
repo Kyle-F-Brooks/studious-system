29d5d8b0b1262954625309963c6ff81896de68fb
kyle-f-brooks <kylefrankbrooks1997@gmail.com>
2018-08-28 10:59:57 +0100

    fixed server

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,8 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email}, you are now a registered user`
-    })
-  })
+  app.post('/register', 
+  AuthenticationControllerPolicy.register,
+  AuthenticationController.register)
 }
